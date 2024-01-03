6db0afaa9616c3834d48dda703dd6c290e84746b
Nattiden <2682645990@qq.com>
2024-01-03 12:45:46 +0000

    lib get card

--- Core/Library.py
+++ Core/Library.py
@@ -11,7 +11,48 @@ class Library:
         self.sub_libraries = {} # 子库
         self.location = os.path.dirname(data['path'])
         self.cards = ScanDireRead(self.location,r'^(?!library\.yaml$).*i')  # 库所拥有的卡片
-        add_sub_libraries(self,EnumDire(self.location,'library.yaml'))  # 遍历添加子库
+        self.add_sub_libraries(EnumDire(self.location,'library.yaml'))  # 遍历添加子库
+
+    def decorateCard(self,card):
+        # 用 fill 和 cover 修饰卡片
+        cloned_fill = self.fill.copy()
+        card.update(self.cover)
+        cloned_fill.update(card)
+        return cloned_fill
+    
+    def getCard(self,card_ref,is_original):
+        target = __getCard_decoless(card_ref,is_original)
+        if is_original:
+            return target
+        else:
+            return self.decorateCard(target)
+
+    def __getCard_decoless(self,card_ref,is_original):
+        previous_cover.update(self.cover)
+        previous_fill.update(self.fill)
+        if ':' in card_ref:
+            splits = card_ref.split(':',2)
+            if splits[0] == self.name:
+                card_ref = splits[1]
+            else:
+                return self.getCardFromLibMapping(splits[0],splits[1],is_original)
+        return self.getCardFromCardMapping(card_ref,is_original)
+
+    def getCardFromCardMapping(self,card_ref,is_original):
+        if card_ref in self.cards.keys():
+            return cards[card_ref].copy()
+        elif card_ref in self.card_mapping:
+            return self.card_mapping[card_ref].getCard(card_ref,is_original)
+        else:
+            # TODO EXPCEPTION CARD NOT FOUND
+            pass
+
+    def getCardFromLibMapping(self,lib_name,card_ref,is_original):
+        if lib_name in self.libs_mapping.keys():
+            return self.libs_mapping[lib_name].getCard(card_ref,is_original)
+        else:
+            # TODO EXCTPTION LIB NOT FOUND
+            pass
 
     def add_sub_libraries(self,yamldata):
         def add_sub_library(self,yamldata):
@@ -19,16 +60,16 @@ class Library:
             self.sub_libraries.update(sublib.name,sublib)
             # 将子库的卡片索引加入父库并映射到该子库
             for cardname in sublib.card_mapping.keys():
-                self.card_mapping.update(cardname,sublib.name)
+                self.card_mapping.update(cardname,sublib)
             # 将子库的所有卡片加入父库并映射到该子库
             for cardname in sublib.cards.keys():
-                self.card_mapping.update(cardname,sublib.name)
+                self.card_mapping.update(cardname,sublib)
             # 将子库的子库引加入父库并映射到该子库
             for libname in sublib.libs_mapping.keys():
-                self.libs_mapping.update(libname,sublib.name)
+                self.libs_mapping.update(libname,sublib)
             # 将该子库加入父库的子库索引
-            self.libs_mapping.update(sublib.name,sublib.name)
-        is yamldata is list:
+            self.libs_mapping.update(sublib.name,sublib)
+        if yamldata is list:
             for data in yaml:
                 add_sub_library(data)
         else:
