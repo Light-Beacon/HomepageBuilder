4fc41f50a75373d00c34d57d3cdea9b76f30874a
Light Beacon <2682645990@qq.com>
2024-02-02 00:56:36 +0800

    fix bugs

--- Example/Libraries/sublibrary/MarkdownTest.md
+++ Example/Libraries/sublibrary/MarkdownTest.md
@@ -1,3 +1,5 @@
 ## H2
-* Item1
+* **Item1**
 * Item2
+
+*[链接](https://www.minecraft.net/zh-hans/article/minecraft-snapshot-24w05b)*
--- Plugin/Markdown/Resources/Scripts/MarkdownPresenter.py
+++ Plugin/Markdown/Resources/Scripts/MarkdownPresenter.py
@@ -2,19 +2,23 @@ import markdown
 from bs4 import BeautifulSoup
 from . import Templates_Manager
 
-def get_replacement(name:str):
+def get_replacement(name:str,attrs:dict):
     component_name = name
     replace_list = []
     if name[0] == 'h' and name[1:].isdigit():
         replace_list.append(('level',name[1:]))
         component_name = 'h'
+    elif name == 'a':
+        replace_list.append(('link',attrs['href']))
     return (component_name, replace_list)
 
-def replace(name,content,res):
+def replace(name,attrs,content,res):
     #print(name)
     components:dict[str,str] = res.components
-    component_name, replace_list = get_replacement(name)
+    component_name, replace_list = get_replacement(name,attrs)
     replace_str = components.get(component_name)
+    if replace_str is None:
+        raise ValueError(f'markdown 中存在尚不支持的元素{name}')
     replace_str = replace_str.replace('${content}',content)
     #print(f'{replace_str} : {content}')
     for k,v in replace_list:
@@ -24,6 +28,7 @@ def replace(name,content,res):
     
 def tag2xaml(tag,res):
     name = tag.name
+    attrs = tag.attrs
     content = ''
     if tag.contents:
         for child in tag.contents:
@@ -31,7 +36,7 @@ def tag2xaml(tag,res):
                 content += child
             else:
                 content += tag2xaml(child,res)
-    return replace(name,content,res)
+    return replace(name,attrs,content,res)
 
 def html2xaml(html,res):
     soup = BeautifulSoup(html,'html.parser')
