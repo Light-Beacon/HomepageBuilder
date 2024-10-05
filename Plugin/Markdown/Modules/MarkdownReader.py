3ae79255dae4e1e08de715d9de5e08a61743572e
Light Beacon <2682645990@qq.com>
2024-10-05 20:44:56 +0800

    using yaml for md

--- Plugin/Markdown/Modules/MarkdownReader.py
+++ Plugin/Markdown/Modules/MarkdownReader.py
@@ -2,7 +2,7 @@
 Markdown 读取模块
 '''
 import re
-import ast
+import yaml
 from Interfaces import file_reader, read_string
 
 # 提取列表项：(?:\[?\s*)(\".*?\"|\'.*?\'|[^,]*?)(?:\s*[,|\]])
@@ -27,17 +27,6 @@ def sep_attr(md):
     attr = {}
     matchs = re.match(ATTR_PATTERN, md)
     if matchs:
-        for attr_str in matchs.groups()[0].split('\n'):
-            if attr_str.startswith('#'):
-                continue
-            if ':' not in attr_str:
-                continue
-            attr_name,attr_value = attr_str.split(':',1)
-            attr_name = attr_name.replace(' ','')
-            attr_value = attr_value.removeprefix(' ')
-            attr_value = attr_value.removesuffix(' ')
-            if attr_value.startswith('[') and attr_value.endswith(']'):
-                attr_value = ast.literal_eval(attr_value)
-            attr[attr_name] = attr_value
+        attr = yaml.safe_load(matchs.groups()[0])
     md = re.sub(ATTR_PATTERN, '', md)
     return md,attr
