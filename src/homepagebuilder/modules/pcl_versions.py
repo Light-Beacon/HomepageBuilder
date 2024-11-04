a05d0ef84bcbcf2ae9738d4e9d5daf3bf70d6ea8
Nattiden <2682645990@qq.com>
2024-11-04 14:23:07 +0800

    fix

--- src/homepagebuilder/modules/pcl_versions.py
+++ src/homepagebuilder/modules/pcl_versions.py
@@ -3,7 +3,7 @@ from homepagebuilder.interfaces import script, format_code, Logger
 logger = Logger('PCLVersionScript')
 
 @script('IF_PCLNewerThan')
-def newer_script(env,versionid,content,**_kwargs):
+def newer_script(versionid,content,env,**_kwargs):
     vid = format_code('${client.versionid}',{},env=env)
     gtid = int(versionid)
     if not vid or isinstance(vid,int):
@@ -14,7 +14,7 @@ def newer_script(env,versionid,content,**_kwargs):
     return ''
 
 @script('IF_PCLOlderThan')
-def lower_script(env,versionid,content,**_kwargs):
+def lower_script(versionid,content,env,**_kwargs):
     vid = format_code('${client.versionid}',{},env=env)
     ltid = int(versionid)
     if not vid or isinstance(vid,int):
