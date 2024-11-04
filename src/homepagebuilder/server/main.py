f075d6b6c843b1f104494b9720ab50fe3eef4f1d
Nattiden <2682645990@qq.com>
2024-11-04 12:33:27 +0800

    fix

--- src/homepagebuilder/server/main.py
+++ src/homepagebuilder/server/main.py
@@ -73,7 +73,7 @@ def getpage(alias:str):
             response_dict = projapi.get_page_json(alias)
             logger.debug(t("server.request.response.json",page=alias,args=args))
         else:
-            response_dict = projapi.get_page_response(alias,client=ClientArgs(request),args)
+            response_dict = projapi.get_page_response(alias,client=ClientArgs(request),args=args)
             logger.debug(t("server.request.response.page",page=alias,args=args))
         response = make_response(response_dict['response'])
         response.headers['Content-Type'] = response_dict['content-type'] 
