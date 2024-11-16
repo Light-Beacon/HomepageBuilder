fa11dc7634c167df4e0b0596cd3271703355561f
Nattiden <2682645990@qq.com>
2024-11-16 21:37:04 +0800

    add docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,10 +14,18 @@ release = '0.14.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc','sphinx_copybutton']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_static_path = ['_static']
+html_css_files = [
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/fontawesome.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/solid.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/brands.min.css",
+    'custom.css'
+]
 
 language = 'zh-cn'
 
@@ -26,4 +34,19 @@ language = 'zh-cn'
 
 #extensions.append("sphinx_wagtail_theme")
 html_theme = 'furo' #press #sphinx_book_theme #sphinx_wagtail_theme #furo
-html_static_path = ['_static']
+
+html_theme_options = {
+    "source_repository": "https://github.com/Light-Beacon/HomepageBuilder",
+    "source_branch": "docs",
+    "source_directory": "docs/",
+    "navigation_with_keys": True,
+    "top_of_page_button": "edit",
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/Light-Beacon/HomepageBuilder",
+            "html": "",
+            "class": "fa-brands fa-solid fa-github fa-2x",
+        },
+    ],
+}
