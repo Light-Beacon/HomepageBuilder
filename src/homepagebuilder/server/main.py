6b5afc06a6a7a81b0720dd579914ffa4f05b425e
Nattiden <2682645990@qq.com>
2024-11-21 19:59:25 +0800

    name change

--- src/homepagebuilder/server/main.py
+++ src/homepagebuilder/server/main.py
@@ -15,7 +15,7 @@ from ..core.config import config
 
 logger = Logger('Server')
 app = Flask(__name__)
-if config('server.proxyfix'):
+if config('Server.ProxyFix'):
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
 projapi = None
 
