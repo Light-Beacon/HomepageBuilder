ab383865053b330831393c81399bba77cafd838d
Nattiden <2682645990@qq.com>
2026-03-21 22:17:04 +0800

    Make text node overridable

--- src/homepagebuilder/plugins/markdown/modules/markdown_parsers.py
+++ src/homepagebuilder/plugins/markdown/modules/markdown_parsers.py
@@ -1,8 +1,8 @@
 from typing import Union, List, Dict, Optional
 from abc import abstractmethod
 from enum import Enum
-import re 
-from homepagebuilder.interfaces import encode_escape,Logger
+import re
+from homepagebuilder.interfaces import encode_escape, Logger
 from homepagebuilder.core.types.context import Context
 
 logger = Logger('Markdown')
@@ -10,43 +10,48 @@ FIRSTLINE_SPACES = '    '
 
 TAG_PARSER_MAPPING = {}
 
+
 def handles(*args):
     def wrapper(cls):
         for name in args:
             TAG_PARSER_MAPPING[name] = cls
         return cls
+
     return wrapper
 
-def find_first_text(node:'Node',regex:Optional[re.Pattern],
-                    remove:bool = False) -> Optional[str]:
+
+def find_first_text(node: 'Node', regex: Optional[re.Pattern],
+                    remove: bool = False) -> Optional[str]:
     '''寻找节点中符合要求的最开始的文本'''
     for child in node.children:
-        if isinstance(child,Text):
-            if len(text := child.content.replace('\n','')) > 0:
+        if isinstance(child, Text):
+            if len(text := child.content.replace('\n', '')) > 0:
                 # 找到了文本
                 if not regex:
                     if remove:
                         child.content = ''
                     return text
                 else:
-                    if matches := re.match(regex,text):
+                    if matches := re.match(regex, text):
                         text = matches.group(1)
                         if remove:
-                            child.content = child.content.replace(matches.group(0),'',1)
+                            child.content = child.content.replace(matches.group(0), '', 1)
                         return text
         else:
-            if text := find_first_text(child,regex,remove):
+            if text := find_first_text(child, regex, remove):
                 return text
     return None
 
-def to_plain_str(node:'Node') -> str:
+
+def to_plain_str(node: 'Node') -> str:
     content = ''
     for child in node.children:
-        if isinstance(child,Text):
+        if isinstance(child, Text):
             content += child.content
         else:
             content += to_plain_str(child)
     return content
+
 
 class NodeType(Enum):
     ANY = -1
@@ -56,16 +61,17 @@ class NodeType(Enum):
     INLINE = 3
     UIELEMENT = 4
 
+
 class Node():
-    def __init__(self,tag,parent_stack):
+    def __init__(self, tag, parent_stack):
         self.context = Context.get_current_context()
         self.components = self.context.components
-        self.name:str = ''
+        self.name: str = ''
         self.attrs = None
         self.tag = tag
-        self.children:List['Node'] = []
-        self.parent_stack:List['Node'] = parent_stack
-        self.expose_children:bool = False
+        self.children: List['Node'] = []
+        self.parent_stack: List['Node'] = parent_stack
+        self.expose_children: bool = False
         """隐藏本元素，将子元素设为与本元素同级"""
 
     @property
@@ -108,7 +114,7 @@ class Node():
         '''解析子元素'''
 
     @abstractmethod
-    def add_child_node(self,child_node:'Node') -> None:
+    def add_child_node(self, child_node: 'Node') -> None:
         '''增加子节点'''
 
     @abstractmethod
@@ -129,6 +135,7 @@ class Node():
         '''本节点的类型'''
         return NodeType.UNDEFINED
 
+
 class VoidNode(Node):
     def get_replacement(self):
         return None
@@ -139,7 +146,7 @@ class VoidNode(Node):
     def parse_children(self):
         pass
 
-    def add_child_node(self, child_node:'Node'):
+    def add_child_node(self, child_node: 'Node'):
         raise NotImplementedError()
 
     def convert_children(self):
@@ -148,7 +155,8 @@ class VoidNode(Node):
     def convert(self) -> str:
         return ''
 
-class NodeBase(Node): 
+
+class NodeBase(Node):
     def __init__(self, tag, *args, **kwargs):
         super().__init__(tag, *args, **kwargs)
         self.escaping_special_chars: bool = False
@@ -171,11 +179,11 @@ class NodeBase(Node):
         component_obj.mark_used_resources(replacement)
         replace_str = str(component_obj)
         if len(replacement) > 0:
-            for k,v in replacement.items():
+            for k, v in replacement.items():
                 replace_str = replace_str.replace(f'${{{k}}}',
-                                                encode_escape(str(v),
-                                                    with_special=self.escaping_special_chars,
-                                                    with_brace=self.escaping_brace))
+                                                  encode_escape(str(v),
+                                                                with_special=self.escaping_special_chars,
+                                                                with_brace=self.escaping_brace))
         return replace_str
 
     def parse_children(self):
@@ -184,7 +192,7 @@ class NodeBase(Node):
             for child in self.tag.contents:
                 self.add_child_node(create_node(tag=child, parent_stack=self.parent_stack + [self]))
 
-    def add_child_node(self,child_node:Node):
+    def add_child_node(self, child_node: Node):
         self.children.append(child_node)
 
     def convert_children(self):
@@ -197,48 +205,60 @@ class NodeBase(Node):
         content = self.convert_children() if self.children else ''
         if self.expose_children:
             return content
-        element_frame:str = self.get_element_frame()
-        return element_frame.replace('${content}',content)
+        element_frame: str = self.get_element_frame()
+        return element_frame.replace('${content}', content)
+
 
 class InlineNode(NodeBase):
     """行内元素"""
+
     @property
     def node_type(self):
         return NodeType.INLINE
+
 
 class BlockNode(NodeBase):
     """块元素"""
+
     @property
     def node_type(self):
         return NodeType.BLOCK
 
+
 class InlineNodeContainer(NodeBase):
     """行内元素容器"""
+
     @property
     def contain_type(self):
         return NodeType.INLINE
 
+
 class BlockNodeContainer(NodeBase):
     """块元素容器"""
+
     @property
     def contain_type(self):
         return NodeType.BLOCK
 
+
 class WPFUIElement(NodeBase):
     """WPF UI 元素"""
+
     @property
     def node_type(self):
         return NodeType.UIELEMENT
 
+
 class WPFUIContainer(NodeBase):
     """WPF UI 自动容器"""
+
     @property
     def node_type(self):
         return NodeType.ANY
 
     def convert(self):
         if not self.actual_ancestor or \
-            self.actual_ancestor.contain_type in [NodeType.BLOCK, NodeType.ANY]:
+                self.actual_ancestor.contain_type in [NodeType.BLOCK, NodeType.ANY]:
             return "<BlockUIContainer>" + super().convert() + "</BlockUIContainer>"
         elif self.actual_ancestor.contain_type == NodeType.INLINE:
             return "<InlineUIContainer>" + super().convert() + "</InlineUIContainer>"
@@ -246,17 +266,21 @@ class WPFUIContainer(NodeBase):
             logger.error(f'Unknown ancestor type: {self.actual_ancestor.contain_type}')
             raise TypeError
 
-@handles('em','strong','del','br')
+
+@handles('em', 'strong', 'del', 'br')
 class CommonInlineNode(InlineNode, InlineNodeContainer):
     """通用行内元素"""
 
-@handles('hr','ul','ol')
+
+@handles('hr', 'ul', 'ol')
 class CommonBlockNode(BlockNode, InlineNodeContainer):
     """通用块元素"""
 
+
+@handles('text-node')
 class Text(VoidNode):
-    def __init__(self,tag,*args,**kwargs):
-        super().__init__(tag,*args,**kwargs)
+    def __init__(self, tag, *args, **kwargs):
+        super().__init__(tag, *args, **kwargs)
         self.content = str(tag)
         self.escaping_special_chars = False
         self.escaping_brace = True
@@ -276,7 +300,7 @@ class Text(VoidNode):
     def node_type(self):
         return NodeType.PLAINTEXT
 
-    def __eq__(self,cmp):
+    def __eq__(self, cmp):
         return cmp == self.content
 
     def __format__(self, format_spec):
@@ -285,19 +309,20 @@ class Text(VoidNode):
     def __str__(self):
         return f'<MarkdownText content="{self.content}">'
 
+
 @handles('p')
 class Paragraph(BlockNode, InlineNodeContainer):
     @classmethod
-    def from_inline_list(cls,inline_list,parent_stack):
-        p = Paragraph(tag = None, parent_stack=parent_stack)
+    def from_inline_list(cls, inline_list, parent_stack):
+        p = Paragraph(tag=None, parent_stack=parent_stack)
         p.children = inline_list
         p.name = 'p'
         p.tag = '<virtual p>'
         return p
 
-    def parse_children(self,*args,**kwargs):
+    def parse_children(self, *args, **kwargs):
         # 尽量让子元素以块状呈现
-        super().parse_children(*args,**kwargs)
+        super().parse_children(*args, **kwargs)
         children_buffer_inline = []
         new_children = []
         replace_children_flag = False
@@ -319,21 +344,24 @@ class Paragraph(BlockNode, InlineNodeContainer):
                 parent_stack=self.parent_stack))
             self.children = new_children
 
+
 class ListItemParagraph(Paragraph):
-    def __init__(self,children, parent_stack ):
-        super().__init__(tag = None, parent_stack=parent_stack)
+    def __init__(self, children, parent_stack):
+        super().__init__(tag=None, parent_stack=parent_stack)
         self.children = children
-    
-    def parse_children(self,*args,**kwargs):
+
+    def parse_children(self, *args, **kwargs):
         pass
 
     @property
     def component_name(self):
         return 'listitempara'
 
+
 @handles('li')
 class MarkdownListItem(BlockNode, BlockNodeContainer):
     children_paragraph_class = ListItemParagraph
+
     def parse_children(self):
         super().parse_children()
         inline_children_buffer = []
@@ -342,7 +370,7 @@ class MarkdownListItem(BlockNode, BlockNodeContainer):
             return
         for child in self.children:
             if child.node_type == NodeType.PLAINTEXT:
-                if isinstance(child,Text) and child.isblank():
+                if isinstance(child, Text) and child.isblank():
                     continue
                 inline_children_buffer.append(child)
             elif child.node_type == NodeType.INLINE:
@@ -351,14 +379,15 @@ class MarkdownListItem(BlockNode, BlockNodeContainer):
                 if len(inline_children_buffer) > 0:
                     new_children.append(self.children_paragraph_class(
                         inline_children_buffer,
-                        parent_stack = self.parent_stack + [self]))
+                        parent_stack=self.parent_stack + [self]))
                     inline_children_buffer = []
                 new_children.append(child)
         if len(inline_children_buffer) > 0:
             new_children.append(self.children_paragraph_class(
                 inline_children_buffer,
-                parent_stack = self.parent_stack + [self]))
+                parent_stack=self.parent_stack + [self]))
         self.children = new_children
+
 
 QUOTE_TYPE_PATTERN = re.compile(r'^\[!(.*)\]')
 QUOTE_TYPE_NAMES = {
@@ -375,9 +404,11 @@ QUOTE_TYPE_ISWARN_MAPPING = {
     None: None
 }
 
+
 @handles('code')
 class InlineCode(InlineNode):
     """行内代码块"""
+
     def __init__(self, tag, *args, **kwargs):
         super().__init__(tag, *args, **kwargs)
         self.escaping_special_chars = True
@@ -386,12 +417,14 @@ class InlineCode(InlineNode):
     def parse_children(self):
         super().parse_children()
         for child in self.children:
-            if isinstance(child,Text):
+            if isinstance(child, Text):
                 child.escaping_brace = False
+
 
 @handles('blockcode')
 class BlockCode(BlockNode):
     """块状代码块"""
+
     def __init__(self, tag, *args, **kwargs):
         super().__init__(tag, *args, **kwargs)
         self.escaping_special_chars = True
@@ -405,6 +438,7 @@ class BlockCode(BlockNode):
         replacements = {'language': self.attrs['lang'],
                         'code': self.attrs['code']}
         return replacements
+
 
 @handles('blockquote')
 class Quote(BlockNode, BlockNodeContainer):
@@ -439,7 +473,8 @@ class Quote(BlockNode, BlockNodeContainer):
             content = super().convert_children()
         return content
 
-@handles('h1','h2','h3','h4','h5')
+
+@handles('h1', 'h2', 'h3', 'h4', 'h5')
 class Heading(BlockNode):
     @property
     def component_name(self) -> str:
@@ -447,6 +482,7 @@ class Heading(BlockNode):
 
     def get_replacement(self) -> Dict[str, str]:
         return {'level': self.name[1:]}
+
 
 class LinkType(Enum):
     homepage = '打开帮助'
@@ -458,6 +494,7 @@ class LinkType(Enum):
     refresh_homepage = '刷新主页'
     download = '下载文件'
     browse = '打开网页'
+
 
 @handles('a')
 class Link(InlineNode, InlineNodeContainer):
@@ -504,10 +541,11 @@ class Link(InlineNode, InlineNodeContainer):
             reps['pos_down'] = 2
         return reps
 
+
 @handles('img')
 class MarkdownImage(WPFUIContainer):
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.title = self.attrs.get('title')
 
     @property
@@ -520,8 +558,9 @@ class MarkdownImage(WPFUIContainer):
             replacements['title'] = self.title
         return replacements
 
-def create_node(tag,parent_stack):
-    if isinstance(tag,str):
-        return Text(tag,parent_stack=parent_stack)
+
+def create_node(tag, parent_stack):
+    if isinstance(tag, str):
+        return TAG_PARSER_MAPPING['text-node'](tag, parent_stack=parent_stack)  # Text
     else:
-        return TAG_PARSER_MAPPING[tag.name](tag=tag,parent_stack=parent_stack)
+        return TAG_PARSER_MAPPING[tag.name](tag=tag, parent_stack=parent_stack)
